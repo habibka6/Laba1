d60ab0f47583fc2b87ac7f1df59c23cdd521989a
habib <sherif@>
2025-02-27 21:53:24 +0300

    Initial commit: CMake project setup

---
+++ src/utils.cpp
@@ -0,0 +1,8 @@
+// Функция add для сложения двух чисел
+int add(int a, int b) {
+    return a + b;
+}
+// Функция  subtract для свычитания второго числа из первого
+int subtract(int a, int b) {
+    return a - b;
+}
